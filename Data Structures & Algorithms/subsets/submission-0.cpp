7579405dd1629904e0f43fe6321e0668e1dd8894
Pratham Gautam <69783810+mehffine@users.noreply.github.com>
2026-04-15 15:59:45 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void solve(vector<int> &nums, vector<int>&temp, int i){
+        if(i>=nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+
+        //take
+        temp.push_back(nums[i]);
+        solve(nums,temp,i+1);
+
+        //not take
+        temp.pop_back();
+        solve(nums,temp,i+1);
+    }
+
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+
+        solve(nums,temp,0);
+
+        return ans;
+    }
+};
