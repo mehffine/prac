b36d3a1d176874c1cbd3f637770c8836a875871e
Pratham Gautam <69783810+mehffine@users.noreply.github.com>
2026-04-17 23:09:57 +0530

    Add: sum-of-all-subset-xor-totals - submission-0

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> temp;
+    vector<vector<int>> ans;
+    int total = 0;
+    int tempi = 0;
+    void solve(vector<int>& nums,vector<vector<int>>& ans,int i){
+        if(i>=nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+
+        //take
+        solve(nums,ans,i+1);
+        temp.push_back(nums[i]);
+        tempi = tempi ^ nums[i];
+        total+= tempi;
+
+        //not take
+        solve(nums,ans,i+1);
+        temp.pop_back();
+
+
+    }
+    int subsetXORSum(vector<int>& nums) {
+        solve(nums,ans,0);
+        
+        int total = 0;
+        for(int i=0; i<ans.size(); i++){
+            int xori = 0;
+            for(int j=0; j<ans[i].size(); j++){
+                xori = xori ^ ans[i][j];
+            }
+            total +=xori;
+        }
+        return total;
+    }
+};
