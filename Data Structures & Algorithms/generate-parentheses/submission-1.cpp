7d3f554eb2d1fbdbd553f7635ac094868c3511cd
Pratham Gautam <69783810+mehffine@users.noreply.github.com>
2026-04-20 16:43:27 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<string> ans;
+    bool isValid(string s) {
+        int n = s.size();
+        stack<char> st;
+        for(int i=0; i<n; i++){
+            if(s[i]=='('){
+                st.push(s[i]);
+            }else{
+                if(st.empty()) return false;
+                char temp = st.top();
+                st.pop();
+                if((s[i]==')' && temp!='(')){
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+
+    void solve(string &s, int n){
+        if(s.size()==n*2){
+            if(isValid(s)){
+                ans.push_back(s);
+            }
+            return;
+        }
+
+        //take (
+        s.push_back('(');
+        solve(s,n);
+        s.pop_back();
+
+        //take )
+        s.push_back(')');
+        solve(s,n);
+        s.pop_back();
+        
+    }
+    vector<string> generateParenthesis(int n) {
+        string s = "";
+        solve(s,n);
+        return ans;
+    }
+};
